082eb717db2778597c6e032c3d6b33971cdef4d3
BhuvaneshKumar <32215672+bhuvanesh42@users.noreply.github.com>
2020-08-14 20:28:03 +0530

    Initial commit

---
+++ lib/dummy_vales.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:meals/Models/category.dart';
+
+const Dummy_values = const [
+  Category(id: 'a1', title: 'indian', color: Colors.blue),
+  Category(id: 'a2', title: 'italian', color: Colors.grey),
+  Category(id: 'a3', title: 'bengali', color: Colors.red),
+  Category(id: 'a4', title: 'indian', color: Colors.lightBlue),
+  Category(id: 'a5', title: 'italian', color: Colors.amber),
+  Category(id: 'a6', title: 'bengali', color: Colors.deepPurple),
+];
